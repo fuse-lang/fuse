e4a8dee6fb12ec676d15b87de548ca6dc2181d45
rzvxa <rzvxa@protonmail.com>
2024-02-27 16:06:31 +0330

    test(parser): better testing infrastructure.

--- crates/fuse-parser/tests/cases/mod.rs
+++ crates/fuse-parser/tests/cases/mod.rs
@@ -1,23 +1,46 @@
-use insta::assert_yaml_snapshot;
 use std::{ffi::OsStr, fs, path::PathBuf};
 
 use fuse_parser::{lexer::Lexer, parse};
+
+struct Context<'a> {
+    root: PathBuf,
+    test_dir: PathBuf,
+    source_name: &'a str,
+    settings: insta::Settings,
+}
+
+impl<'a> Context<'a> {
+    fn path(&self) -> PathBuf {
+        self.root.join(&self.test_dir)
+    }
+
+    fn settings(&self) -> insta::Settings {
+        self.settings.clone()
+    }
+}
 
 #[test]
 fn pass() {
     let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 
+    let ctx = Context {
+        root,
+        test_dir: PathBuf::new().join("tests").join("cases").join("pass"),
+        source_name: "case.fuse",
+        settings: insta::Settings::clone_current(),
+    };
+
     // TODO: better way for pointing to the test cases directory.
-    let cases = load_cases(&root.join("tests").join("cases").join("pass"));
+    let cases = load_cases(&ctx);
 
     for case in cases {
-        run(case, false, false);
+        run(&ctx, case, false, false);
     }
 }
 
 // helpers
-fn load_cases(dir: &PathBuf) -> Vec<PathBuf> {
-    fs::read_dir(dir)
+fn load_cases(ctx: &Context) -> Vec<PathBuf> {
+    fs::read_dir(ctx.path())
         .unwrap()
         .map(|x| x.unwrap())
         .filter(|x| x.metadata().is_ok_and(|meta| meta.is_dir()))
@@ -25,11 +48,17 @@ fn load_cases(dir: &PathBuf) -> Vec<PathBuf> {
         .collect()
 }
 
-fn run(path: PathBuf, expect_error: bool, expect_panic: bool) {
-    let path = path.join("case.fuse");
+fn run(ctx: &Context, case_dir: PathBuf, expect_error: bool, expect_panic: bool) {
+    let mut settings = ctx.settings();
+    let source_path = case_dir.join(ctx.source_name);
 
-    test_lexer(path.as_os_str());
-    test_parser(path.as_os_str(), expect_error, expect_panic);
+    settings.set_input_file(&source_path);
+    settings.set_snapshot_path(case_dir);
+    settings.set_prepend_module_to_snapshot(false);
+
+    let _guard = settings.bind_to_scope();
+    test_lexer(source_path.as_os_str());
+    test_parser(source_path.as_os_str(), expect_error, expect_panic);
 }
 
 fn test_lexer(src_path: &OsStr) {
@@ -42,7 +71,7 @@ fn test_lexer(src_path: &OsStr) {
         "Lexer changes the content of the original source buffer."
     );
 
-    assert_yaml_snapshot!("tokens", tokens);
+    insta::assert_yaml_snapshot!("tokens", tokens);
 }
 
 fn test_parser(path: &OsStr, expect_error: bool, expect_panic: bool) {
@@ -70,5 +99,5 @@ fn test_parser(path: &OsStr, expect_error: bool, expect_panic: bool) {
     let chunk = parsed.chunk;
     let errors = parsed.errors;
 
-    assert_yaml_snapshot!("ast", chunk);
+    insta::assert_yaml_snapshot!("ast", chunk);
 }

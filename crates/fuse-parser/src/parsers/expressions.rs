0b95e37b80f48c1822c6ca6075ae2587e2a6294d
rzvxa <rzvxa@protonmail.com>
2024-03-04 11:42:08 +0330

    refactor(parser): better function param parsing.

--- crates/fuse-parser/src/parsers/expressions.rs
+++ crates/fuse-parser/src/parsers/expressions.rs
@@ -1,5 +1,9 @@
 use crate::{lexer::TokenKind, Parser, ParserResult};
-use fuse_ast::{BooleanLiteral, Expression, Function, FunctionParameters, Identifier};
+use fuse_ast::{
+    BindingPattern, BindingPatternKind, BooleanLiteral, Expression, Function, FunctionParameter,
+    FunctionParameters, Identifier,
+};
+use fuse_common::Span;
 
 impl<'a> Parser<'a> {
     pub(crate) fn parse_expression(&mut self) -> ParserResult<Expression> {
@@ -37,14 +41,46 @@ impl<'a> Parser<'a> {
         let start = self.start_span();
         // Consume the keyword
         self.consume();
-        self.consume_expect(TokenKind::RParen)?;
+        let params = self.parse_function_parameters()?;
         Ok(Function {
             span: self.end_span(start),
-            params: FunctionParameters {
-                span: todo!(),
-                items: todo!(),
-                rest: todo!(),
-            },
+            params,
         })
+    }
+
+    fn parse_function_parameters(&mut self) -> ParserResult<FunctionParameters> {
+        let rparen = self.consume_expect(TokenKind::RParen)?;
+        // Empty function parameters
+        if let Some(lparen) = self.consume_if(TokenKind::LParen) {
+            return Ok(FunctionParameters { span: Span::new(rparen.start(), lparen.end()), items: Vec::new(), rest: None })
+        }
+
+        let mut params: Vec<FunctionParameter> = Vec::new();
+        while self.at(TokenKind::Identifier) {
+            let binding = self.parse_binding()?;
+            match &binding {
+                BindingPattern {
+                    kind: BindingPatternKind::Identifier(kind),
+                    ..
+                } => params.push(FunctionParameter {
+                    span: kind.span,
+                    pattern: binding,
+                }),
+                _ => todo!(
+                    "Diagnosis error here,\
+                            we don't allow variable deconstruction for function parameters."
+                ),
+            }
+        }
+
+        let lparen = self.consume_expect(TokenKind::LParen)?;
+        Ok(
+            FunctionParameters {
+                span: Span::new(rparen.start(), lparen.end()),
+                items: params,
+                rest: None,
+            }
+        )
+
     }
 }
